b751fb153bfe91bde6aa90f0148b92fb5c6ce39e
Vaibhav Kashyap <vaibhavkashyap230@gmail.com>
2020-02-26 10:23:00 +0530

    Create nodes at k distance.java

---
+++ nodes at k distance.java
@@ -0,0 +1,18 @@
+class GfG
+{
+     // Recursive function to print right view of a binary tree.
+     void printKdistance(Node root, int k)
+     {
+         if(root==null)
+         return;
+         
+         if(k==0)
+        {
+             System.out.print(root.data+" ");
+             return;
+        }
+        
+        printKdistance(root.left,k-1);
+        printKdistance(root.right,k-1);
+     }
+}
