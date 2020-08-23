22f8d3b22a41dd66918d1414bce58d3ccf46e114
Vaibhav Kashyap <vaibhavkashyap230@gmail.com>
2020-08-23 14:46:24 +0530

    Create Shortest distance in infinite tree.java

---
+++ Shortest distance in infinite tree.java
@@ -0,0 +1,30 @@
+// https://practice.geeksforgeeks.org/problems/find-the-distance-between-two-nodes/0/
+
+import java.util.*;
+class GFG
+ {
+	public static void main (String[] args)
+	 {
+     	Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while(test-->0)
+        {
+            int a = sc.nextInt(), b = sc.nextInt(), count = 0;
+            while(a!=b)
+            {
+                while(a>b)
+                {
+                    a/=2;
+                    count++;
+                }
+                while(b>a)
+                {
+                    b/=2;
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+        sc.close();
+	 }
+}https://practice.geeksforgeeks.org/problems/find-the-distance-between-two-nodes/0/
